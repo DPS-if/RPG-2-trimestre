6d467192f16955f08ab26c519ed8d5a061dd00b0
Gabriel <gabriel.franklin@estudante.iftm.edu.br>
2026-06-01 22:39:56 -0300

    esboço tela inicial completo

--- TelaInicial.cs
+++ TelaInicial.cs
@@ -7,5 +7,20 @@ public partial class TelaInicial : Control
 	{
 		var quitBtn = GetNode<TextureButton>("MarginContainer/HBoxContainer/VBoxContainer/quit_btn");
 		quitBtn.Pressed += () => GetTree().Quit();
+		var startBtn = GetNode<TextureButton>("MarginContainer/HBoxContainer/VBoxContainer/start_btn");
+		startBtn.Pressed += () =>
+		{
+			GetTree().ChangeSceneToFile("res://fase1.tscn");
+		};
+		var creditsBtn = GetNode<TextureButton>("MarginContainer/HBoxContainer/VBoxContainer/creditos_btn");
+		creditsBtn.Pressed += () =>
+		{
+			GetTree().ChangeSceneToFile("res://creditos.tscn");
+		};
+		var configBtn = GetNode<TextureButton>("MarginContainer/HBoxContainer/VBoxContainer/config_btn");
+		configBtn.Pressed += () =>
+		{
+			GetTree().ChangeSceneToFile("res://config.tscn");
+		};
 	}
 }
